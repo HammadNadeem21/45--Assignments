c38020e1d86de9911304e4d1b948249a631df295
HammadNadeem21 <nhammad424@gmail.com>
2024-06-17 21:22:09 +0500

    updated

---
+++ Q-14/Q14.js
@@ -0,0 +1,6 @@
+// 14. Guest List: If you could invite anyone, living or deceased, to dinner, who would you invite? Make a list that includes at least three people you’d like to
+// invite to dinner. Then use your list to print a message to each person, inviting them to dinner.
+var guest_list = ["Hammad", "Taha", "Sufyan", "Huzaifa"];
+for (var i = 0; i < guest_list.length; i++) {
+    console.log("Dear ".concat(guest_list[i], " \n\tyou are invited to dinner. Please join us for an evening of good food and great conversation. \nThank you"));
+}
