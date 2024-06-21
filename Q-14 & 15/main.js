fe15c47e1074a758923b1ebf148f71ac78a1adb2
HammadNadeem21 <nhammad424@gmail.com>
2024-06-21 11:43:55 +0500

    updated

---
+++ Q-14 & 15/main.js
@@ -0,0 +1,22 @@
+// 14. Guest List: If you could invite anyone, living or deceased, to dinner, who would you invite? Make a list that includes at least three people you’d like to
+// invite to dinner. Then use your list to print a message to each person, inviting them to dinner.
+var guest_list = ["Hammad", "Taha", "Sufyan", "Huzaifa"];
+for (var i = 0; i < guest_list.length; i++) {
+    console.log("\nDear ".concat(guest_list[i], " \nyou are invited to dinner. Please join us for an evening of good food and great conversation. \nThank you"));
+}
+// Q-15. Changing Guest List: You just heard that one of your guests can’t make the
+// dinner, so you need to send out a new set of invitations. You’ll have to think of
+// someone else to invite.
+// • Start with your program from Exercise 14. Add a print statement at the
+// end of your program stating the name of the guest who can’t make it.
+// • Modify your list, replacing the name of the guest who can’t make it with
+// the name of the new person you are inviting.
+// • Print a second set of invitation messages, one for each person who is still
+// in your list.
+var not_present = "Huzaifa";
+console.log("Unfortunately, ".concat(not_present, " can't make it to the dinner."));
+var new_guest = "Adnan";
+guest_list[3] = new_guest;
+for (var i = 0; i < guest_list.length; i++) {
+    console.log("\nDear ".concat(guest_list[i], " \nyou are invited to dinner. Please join us for an evening of good food and great conversation. \nThank you"));
+}
