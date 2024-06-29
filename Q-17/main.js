4d5c048c9ef59954aebed90e32b2c99e8072bced
HammadNadeem21 <nhammad424@gmail.com>
2024-06-29 07:01:20 +0500

    updated

--- Q-17/main.js
+++ Q-17/main.js
@@ -9,69 +9,43 @@
 
  • Remove the last two names from your list, so you have an empty list. Print your list to make sure you actually have an empty list at the end
  of your program.*/
-
-
 // Exercise:16
-
- let guest_list:string[] = ["Taha", "Sufyan","Huzaifa"];
-
+var guest_list = ["Taha", "Sufyan", "Huzaifa"];
 /*for(let i = 0; i < guest_list.length; i++){
 console.log(`\nDear ${guest_list[i]}\nyou are invited to dinner. Please join us for an evening of good food and great conversation. \nThank you`)
 }*/
-
-
-let not_present:string = "Huzaifa";
-console.log(`\nUnfortunately, ${not_present} can't make it to the dinner.`);
-
-guest_list.splice(2,2,"Adnan")
-
-
+var not_present = "Huzaifa";
+console.log("\nUnfortunately, ".concat(not_present, " can't make it to the dinner."));
+guest_list.splice(2, 2, "Adnan");
 /*for(let i = 0; i < guest_list.length; i++){
     console.log(`\nDear ${guest_list[i]} \nyou are invited to dinner. Please join us for an evening of good food and great conversation. \nThank you`)
     }*/
-
-
 console.log("\nWe found a bigger dinner table!");
-
-
 // first guest
 guest_list.unshift("Salman");
-
 // second guest
-let middleGuest = Math.floor(guest_list.length /2)
+var middleGuest = Math.floor(guest_list.length / 2);
 guest_list.splice(middleGuest, 0, "Sameer");
-
 // third guest
 guest_list.push("Ikhlas");
-
-
-
 /*for(let i = 0; i<guest_list.length; i++){
 console.log(`\nDear ${guest_list[i]}, please join us for dinner. \nThank you`)
 }*/
-
 console.log(guest_list);
-
-
 // Exercise 17
 console.log("\nUnfortunately we can not arrange big dinner tablr, only two guests can be invited.");
-
 // remove guest
-while(guest_list.length > 2){
-let removeGuest:string | undefined = guest_list.pop();
-if(removeGuest !== undefined){
-console.log(`Sorry, ${removeGuest} we can no invite you.`);
+while (guest_list.length > 2) {
+    var removeGuest = guest_list.pop();
+    if (removeGuest !== undefined) {
+        console.log("Sorry, ".concat(removeGuest, " we can no invite you."));
+    }
 }
-}
-
 // print a message to each of the two people still on your list
-for(let i = 0; i<guest_list.length; i++ ){
-console.log(`Dear ${guest_list} you are invited for a dinner.`);
+for (var i = 0; i < guest_list.length; i++) {
+    console.log("Dear ".concat(guest_list, " you are invited for a dinner."));
 }
-
 // empty guest list
 guest_list.splice(0, guest_list.length);
-
 // print empty guestlist
 console.log(guest_list);
-
