5d1e9c95ae64847a727835ebc6855d906b0b1b0c
HammadNadeem21 <nhammad424@gmail.com>
2024-08-21 22:55:31 +0500

    updated

--- Q-22/Q22.js
+++ Q-22/Q22.js
@@ -1,10 +1,9 @@
+"use strict";
 // Q)22. Intentional Error: If you haven’t received an array index error in one of your programs yet, try to make one happen. Change an index in one of your programs
 // to produce an index error. Make sure you correct the error before closing the program.
-
-let errorArray: (string | number) [] = ["Hammad", 23, "Hamid", 78, 98]
-
+Object.defineProperty(exports, "__esModule", { value: true });
+let errorArray = ["Hammad", 23, "Hamid", 78, 98];
 // Produucing Error by accessing invalid index of array
 console.log(errorArray[6]);
-
 // Error Removed
 console.log(errorArray[3]);
