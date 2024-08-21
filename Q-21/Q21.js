a70aa5bdbab9a6ca6dac4c1accae32a329213b43
HammadNadeem21 <nhammad424@gmail.com>
2024-08-21 19:55:06 +0500

    updated

---
+++ Q-21/Q21.js
@@ -0,0 +1,8 @@
+//Q)21: They think of something you could store in a TypeScript Object. Write a program that creates Objects containing these items.
+var student = {
+    Name: "Hammad",
+    Id: 183727,
+    Location: "Karachi",
+    Email: "hamm426623@gmail.com"
+};
+console.log(student);
---
+++ Q-21/Q21.ts
@@ -0,0 +1,16 @@
+//Q)21: They think of something you could store in a TypeScript Object. Write a program that creates Objects containing these items.
+
+interface info {
+    Name:string,
+    Id:number,
+    Location:string,
+    Email:string
+}
+
+let student:info ={
+    Name:"Hammad",
+    Id:183727,
+    Location:"Karachi",
+    Email:"hamm426623@gmail.com"
+}
+console.log(student);
